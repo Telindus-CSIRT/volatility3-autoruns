df6b7112d90ddedd5a7edd3750256039cb2bcbd0
garanews <puntogtg@tiscali.it>
2022-04-27 08:13:45 +0200

    update framework version
    In order to run on latest volatility 3, needs to have framework v 2

--- autorun.py
+++ autorun.py
@@ -145,7 +145,7 @@ class Autoruns(interfaces.plugins.PluginInterface):
     process_dict = {}
     
 
-    _required_framework_version = (1, 0, 0)
+    _required_framework_version = (2, 0, 0)
     @classmethod
     def get_requirements(cls):
         return [requirements.TranslationLayerRequirement(name = 'primary',
